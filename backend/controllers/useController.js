620cfbbfc4a4d83ac80d53d2c6d2293a3898454e
hakan.basturknewinn.com <forxynet@gmail.com>
2025-02-09 01:02:08 +0300

    added jwt to user signup

--- backend/controllers/useController.js
+++ backend/controllers/useController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id, email) => {
+  return jwt.sign({ _id, email }, process.env.SECRET, { expiresIn: '1d' })
+}
 
 // login user
 const loginUser = async (req, res) => {
@@ -10,7 +15,11 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body
   try {
     const user = await User.signup(email, password)
-    res.status(200).json({ email, user })
+
+    //create a token 
+    const token = createToken(user._id, user.email)
+
+    res.status(200).json({ email, token })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
