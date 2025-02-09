dbca6bfcb3eb8de9c36ac35c25ecef22d719d56c
hakan.basturknewinn.com <forxynet@gmail.com>
2025-02-09 18:47:38 +0300

    added usesign and uselogout

--- backend/controllers/useController.js
+++ backend/controllers/useController.js
@@ -1,13 +1,23 @@
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
-const createToken = (_id, email) => {
-  return jwt.sign({ _id, email }, process.env.SECRET, { expiresIn: '1d' })
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1d' })
 }
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ msg: 'login user' });
+  const { email, password } = req.body
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
 }
 
 // signup user
@@ -17,7 +27,7 @@ const signupUser = async (req, res) => {
     const user = await User.signup(email, password)
 
     //create a token 
-    const token = createToken(user._id, user.email)
+    const token = createToken(user._id)
 
     res.status(200).json({ email, token })
   } catch (error) {
