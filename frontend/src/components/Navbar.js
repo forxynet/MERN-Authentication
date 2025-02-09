8b99d7b6aedc5ad795e7006c263f0b892a729195
hakan.basturknewinn.com <forxynet@gmail.com>
2025-02-09 23:39:40 +0300

    added authentication middleware logic

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
+
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleClick = () => {
     logout();
@@ -15,14 +18,16 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
-          <div>
+          {user && (<div>
+            <span className='email'>{user.email}</span>
             <button onClick={handleClick}>Logout</button>
-
           </div>
-          <div>
+          )}
+          {!user && (<div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
           </div>
+          )}
         </nav>
       </div>
     </header>
