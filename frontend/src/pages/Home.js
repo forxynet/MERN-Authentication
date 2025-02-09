c19631985a576e03d676b166ace03ba2b2b50ac1
hakan.basturknewinn.com <forxynet@gmail.com>
2025-02-10 02:12:29 +0300

    updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 import { useAuthContext } from '../hooks/useAuthContext'
 
